c12d088fefa874b145b50843ed93f54ccc40e722
Cristian Duguet <duguet@gmail.com>
2023-01-07 16:15:33 +0100

    adding deck

--- nbdev_cards2/_modidx.py
+++ nbdev_cards2/_modidx.py
@@ -5,6 +5,10 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://cduguet.github.io',
                 'git_url': 'https://github.com/cduguet/nbdev_cards2',
                 'lib_path': 'nbdev_cards2'},
-  'syms': { 'nbdev_cards2.core': { 'nbdev_cards2.core.Card': ('cards.html#card', 'nbdev_cards2/core.py'),
-                                   'nbdev_cards2.core.Card.__init__': ('cards.html#card.__init__', 'nbdev_cards2/core.py'),
-                                   'nbdev_cards2.core.Card.__repr__': ('cards.html#card.__repr__', 'nbdev_cards2/core.py')}}}
+  'syms': { 'nbdev_cards2.card': { 'nbdev_cards2.card.Card': ('card.html#card', 'nbdev_cards2/card.py'),
+                                   'nbdev_cards2.card.Card.__eq__': ('card.html#card.__eq__', 'nbdev_cards2/card.py'),
+                                   'nbdev_cards2.card.Card.__gt__': ('card.html#card.__gt__', 'nbdev_cards2/card.py'),
+                                   'nbdev_cards2.card.Card.__init__': ('card.html#card.__init__', 'nbdev_cards2/card.py'),
+                                   'nbdev_cards2.card.Card.__lt__': ('card.html#card.__lt__', 'nbdev_cards2/card.py'),
+                                   'nbdev_cards2.card.Card.__repr__': ('card.html#card.__repr__', 'nbdev_cards2/card.py')},
+            'nbdev_cards2.deck': {'nbdev_cards2.deck.Deck': ('deck.html#deck', 'nbdev_cards2/deck.py')}}}
